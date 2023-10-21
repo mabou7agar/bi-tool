e869b2f03ee91d83cac0234d4efe0bfec4edd6a0
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2023-10-21 17:56:32 +0300

    Add Customer Seeder

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\CustomerFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\UniqueConstraintViolationException;
+
+class CustomerSeeder extends Seeder
+{
+    public function __construct(private CustomerFactory $customerFactory)
+    {
+    }
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $names = ['C1', 'C2', 'C3', 'C4'];
+        try {
+            $this->customerFactory->count(count($names))
+                ->sequence(fn(Sequence $sequence) => ['name' => $names[$sequence->index]])->create();
+        }
+        catch (UniqueConstraintViolationException $uniqueConstraintViolationException){
+            //No Need to recreated if exists
+        }
+    }
+}
