5296a0cb740680dc2b4a77d6b14f9117300e645f
mabou7agar <13303169+mabou7agar@users.noreply.github.com>
2023-10-23 02:45:52 +0300

    Create CreateRateHistoryHandler

---
+++ app/Handlers/CreateRateHistoryHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handlers;
+
+use App\Models\HotelStayRate;
+use App\Models\HotelStayRatesHistory;
+use Illuminate\Support\Facades\DB;
+
+use function now;
+use function today;
+
+class CreateRateHistoryHandler
+{
+    public function __construct()
+    {
+    }
+
+    public function handle()
+    {
+        $historyTable = (new HotelStayRatesHistory)->getTable();
+        $rateTable = (new HotelStayRate)->getTable();
+        $today = today()->format('Y-m-d');
+        $now = now();
+        $count = HotelStayRatesHistory::where('date_scraped', $today)->count();
+        if ($count < 1) {
+            DB::statement(
+                "INSERT INTO {$historyTable} (id,hotel_name,date_of_stay,date_scraped,rate_per_night,old_uuid,created_at,updated_at) SELECT
+            uuid(),hotel_name,date_of_stay,date_scraped,rate_per_night,id,'{$now}','{$now}' from {$rateTable}"
+            );
+        }
+    }
+}
